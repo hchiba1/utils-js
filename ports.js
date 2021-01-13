00b3445bb5e323da066330c5782b9565206aa554
Hirokazu Chiba <hchiba1@gmail.com>
2021-01-13 11:51:01 +0900

    update ports.js

--- ports.js
+++ ports.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const child_process = require('child_process');
+const columnify = require('columnify');
+const csvParse = require('csv-parse/lib/sync');
 // fs = require('fs');
 // spfmt = require('../lib/spfmt.js');
 
@@ -12,6 +14,32 @@ const commander = require('commander')
 
 commander.parse(process.argv);
 
-// const result = swawnSync('ss');
-const result = child_process.execSync('ss -atunp').toString();
-console.log(result);
+let result = child_process.execSync('ss -atunp').toString();
+// result = result.replace(/ Address/, 'Address');
+// result = result.replace(/ Address/, 'Address');
+// result = result.replace(/ +/g, '\t');
+
+const results = result.trim().split('\n');
+let headers = results.shift().trim()
+    .replace(/ Address/, 'Address')
+    .replace(/ Address/, 'Address')
+    .split(/ +/);
+
+let tsv = headers.join('\t') + '\n';
+
+results.forEach(line => {
+  fields = line.trim().split(/ +/);
+  const length_diff = headers.length - fields.length;
+  tsv += fields.join('\t');
+  if (length_diff) {
+    tsv += '\t';
+  }
+  tsv += '\n';
+});
+
+tsv = columnify(csvParse(tsv, { columns: true, delimiter: '\t', relax: true}), {
+  showHeaders: true,
+  headingTransform: (x) => x
+}).replace(/\s+$/gm, '');
+
+process.stdout.write(tsv);
